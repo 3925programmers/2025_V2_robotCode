25b2bbdf94a5d2196b1dd3b680babaa4e3cbe2fa
craftyclawboom <88505848+craftyclawboom@users.noreply.github.com>
2025-10-14 13:47:05 -0700

    Robocodev1

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+public final class Constants {
+  public final class ElevatorConstants {
+    public static final IdleMode IDLE_MODE = IdleMode.kBrake;
+    public static final boolean INVERTED = false;
+    public static final int CURRENT_LIMIT = 60;
+
+    public final class Control {
+      // public static final double kS = 0.03;
+      // public static final double kG = 0.1;
+      // public static final double kV = 0;
+      // public static final double kP = 7;
+      // public static final double kI = 0;
+      // public static final double kD = 0;
+      // public static final double CRUISE_VELOCITY = 1.5;
+      // public static final double ACCELERATION = 0.7;
+    }
+  }
+
+}
---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,22 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+// import com.revrobotics.spark.SparkMax;
+// import com.revrobotics.spark.SparkBase.ControlType;
+// import com.revrobotics.spark.SparkBase.PersistMode;
+// import com.revrobotics.spark.SparkBase.ResetMode;
+// import com.revrobotics.spark.SparkLowLevel.MotorType;
+// import com.revrobotics.spark.config.SparkMaxConfig;
+// import edu.wpi.first.math.controller.ElevatorFeedforward;
+// import edu.wpi.first.math.controller.ProfiledPIDController;
+// import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import frc.robot.Constants;
+
+// public class Elevator extends SubsystemBase {
+//   SparkMax main_Motor;
+
+
+// }
